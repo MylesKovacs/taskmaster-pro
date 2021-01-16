d96c3810634e881d6ae7a4bccf112f835982ac82
Smack <kovacsmyles1@gmail.com>
2021-01-16 14:10:29 -0600

    added dragging and dropping for both PC and mobile users

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -223,6 +223,22 @@ console.log(tempArr);
   }
 });
 
+// Make trash droppable
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
